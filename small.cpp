e6fc001d4c144802e0c1121ed0db6aeeded73620
oceannoc <23020795@vnu.edu.vn>
2024-02-02 16:43:48 +0700

    Create small.cpp
    file c++

---
+++ small.cpp
@@ -0,0 +1,25 @@
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+
+int main()
+{
+   string a; 
+   cout<<"please type your name";
+   cin >> a;
+   cout<<"Hi "<< a; 
+   cout<<"\n"<<"Have a nice day! ~~";
+
+    
+
+
+}
+
+
+
